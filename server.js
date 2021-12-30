ba3efb699907e7053a0a379b31d5d90118bfae80
Chaostradamus <71539742+Chaostradamus@users.noreply.github.com>
2021-12-30 03:14:46 -0500

    done

--- server.js
+++ server.js
@@ -6,4 +6,14 @@ const app = express();
 
 app.use(cors());
 
+app.get('/api', function(req, res)  {
+    console.log('Called');
+    res.send({result: 'Helloooo'})
+})
+
+app.get('/quit', function(req, res)  {
+    console.log('Called quit');
+    res.send({result: 'Goodbye'})
+})
+
 app.listen(API_PORT, () => console.log(`listening on port ${API_PORT}`));
